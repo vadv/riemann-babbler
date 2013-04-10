96e8d6714bf1c2d749b52fb6401d8c8cda542e92
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-04-10 12:49:43 +0400

    fixes in la screen

--- lib/riemann/babbler/plugins/la.rb
+++ lib/riemann/babbler/plugins/la.rb
@@ -1,7 +1,7 @@
 class Riemann::Babbler::La < Riemann::Babbler
 
   def collect
-    { :service => plugin.service + "la_1", :metric => File.read('/proc/loadavg').split(/\s+/)[2].to_f }
+    { :service => plugin.service + " la_1", :metric => File.read('/proc/loadavg').split(/\s+/)[2].to_f }
   end
 
 end
