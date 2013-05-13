810e2c6063d27e4db37b01e7fb32dba76ac9f910
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-05-14 02:38:58 +0400

    release

--- lib/riemann/babbler/plugins/dummy.rb
+++ lib/riemann/babbler/plugins/dummy.rb
@@ -2,10 +2,10 @@ class Riemann::Babbler::Dummy < Riemann::Babbler
 
   def collect
     if tcp_port_aviable?(configatron.riemann.host, configatron.riemann.port)
-      logger.error "Riemann state 'ok' host: #{configatron.riemann.host}, port #{configatron.riemann.port}"
+      logger.error "Riemann state 'ok' host: #{configatron.riemann.host}, port #{configatron.riemann.port}, proto tcp"
       []
     else
-      logger.error "Can't access to riemann host: #{configatron.riemann.host}, port #{configatron.riemann.port}" 
+      logger.error "Can't access to riemann host: #{configatron.riemann.host}, port #{configatron.riemann.port}, proto tcp" 
     end
     Array.new
   end
