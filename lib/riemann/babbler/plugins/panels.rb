7a0693a24dc93cb8a6056605050277ad1a0c42a3
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-05-27 20:48:38 +0400

    new plugins

---
+++ lib/riemann/babbler/plugins/panels.rb
@@ -0,0 +1,27 @@
+require 'json'
+
+class Riemann::Babbler::Panels < Riemann::Babbler
+
+  def init
+    plugin.set_default(:service, 'panels')
+    plugin.set_default(:interval, 60)
+    plugin.set_default(:url, 'http://localhost/panel.json')
+    plugin.states.set_default(:critical, 60)
+  end
+
+  def collect
+
+    json = JSON.parse rest_get(plugin.url)
+    good, bad = 0, 0
+
+    json["panels"].each do |panel|
+      panel["status"] == "offline" ? bad += 1 : good += 1
+    end
+
+    [
+      {:service => plugin.service + " online", :description => "online panels in #{plugin.url}", :metric => good, :state => 'ok' },
+      {:service => plugin.service + " offline", :description => "offline panels in #{plugin.url}", :metric => bad }
+    ]
+  end
+
+end
--- lib/riemann/version.rb
+++ lib/riemann/version.rb
@@ -1,5 +1,5 @@
 module Riemann
   class Babbler
-    VERSION = '0.9.9.6'
+    VERSION = '0.9.9.7'
   end
 end
