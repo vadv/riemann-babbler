35cc94ed9c1402faa153881e962fc16690eb7e0b
verm666 <verm666@gmail.com>
2013-05-28 14:18:52 +0400

    [feature] Add new plugin: http.rb

---
+++ lib/riemann/babbler/plugins/http.rb
@@ -0,0 +1,37 @@
+class Riemann::Babbler::Http < Riemann::Babbler
+
+  def init
+    plugin.set_default(:service, 'http check')
+    plugin.set_default(:interval, 60)
+
+    plugin.states.set_default(:critical, 1)
+
+    plugin.set_default(:http_code, 200)
+    plugin.set_default(:http_method, 'GET')
+    plugin.set_default(:connect_timeout, 5)
+    plugin.set_default(:retry, 0)
+    plugin.set_default(:retry_delay, 0)
+    plugin.set_default(:max_time, 10)
+   
+    plugin.set_default(:url, 'http://127.0.0.1:80')
+  end
+
+  def collect
+    command = "curl -X#{plugin.http_method} -s"
+    command += " --connect-timeout #{plugin.connect_timeout}"
+    command += " -w '%{http_code}\\n'"
+    command += " --retry #{plugin.retry} --retry-delay #{plugin.retry_delay}"
+    command += " --max-time #{plugin.max_time} --fail"
+    command += " #{plugin.url} -o /dev/null"
+
+    out = shell(command)
+
+    if out.to_i != plugin.http_code
+      metric = 1
+    else
+      metric = 0
+    end
+
+    {:service => plugin.service, :metric => metric, :description => "http_code: #{out}"}
+  end
+end
