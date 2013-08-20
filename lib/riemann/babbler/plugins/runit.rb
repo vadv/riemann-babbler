f4fdef1ae229bfc4eb1cfe3058f9b3ffc4abb451
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-08-20 12:20:33 +0400

    new release

--- lib/riemann/babbler/plugins/runit.rb
+++ lib/riemann/babbler/plugins/runit.rb
@@ -3,7 +3,7 @@ class Riemann::Babbler::Runit < Riemann::Babbler
   def init
     plugin.set_default(:service, 'runit')
     plugin.set_default(:not_monit, ['riemann-client'])
-    plugin.set_default(:warning_uptime, 10)
+    plugin.set_default(:uptime, 10)
     plugin.set_default(:interval, 60)
   end
 
@@ -26,10 +26,8 @@ class Riemann::Babbler::Runit < Riemann::Babbler
       stat_file = File.join(srv, 'supervise', 'stat')
       next unless File.exists? stat_file
       srv_uptime = uptime(srv)
-      if (File.read( stat_file ).strip == 'run') && (srv_uptime > plugin.interval)
+      if (File.read( stat_file ).strip == 'run') && (srv_uptime > plugin.uptime)
         status << {:service => plugin.service + human_srv , :state => 'ok', :description => "runit service #{human_srv} running", :metric => srv_uptime}
-      elsif (File.read( stat_file ).strip == 'run') && (srv_uptime < plugin.warning_uptime)
-        status << {:service => plugin.service + human_srv , :state => 'warning', :description => "runit service #{human_srv} small uptime", :metric => srv_uptime}
       else
         status << {:service => plugin.service + human_srv , :state => 'critical', :description => "runit service #{human_srv} not running", :metric => srv_uptime}
       end
--- lib/riemann/babbler/version.rb
+++ lib/riemann/babbler/version.rb
@@ -1,5 +1,5 @@
 module Riemann
   class Babbler
-    VERSION = '1.2.1'
+    VERSION = '1.2.2'
   end
 end
