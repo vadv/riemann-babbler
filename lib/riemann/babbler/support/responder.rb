5111021a64f60236f162ceb1d58cdd731f83d11c
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-08-20 20:17:04 +0400

    fixes for responder

--- lib/riemann/babbler/support/responder.rb
+++ lib/riemann/babbler/support/responder.rb
@@ -3,21 +3,26 @@ require 'json'
 
 class Riemann::Responder
 
-  INFO = {
-      :version => Riemann::Babbler::VERSION,
-      :ruby => "#{RUBY_VERSION}-#{RUBY_PATCHLEVEL}"
-  }.freeze
-
   def initialize(port = 55755, logger)
     @port = port
     @logger = logger
+    @started_at = Time.now.to_i
+  end
+
+  def info
+    {
+      :version => Riemann::Babbler::VERSION,
+      :ruby => "#{RUBY_VERSION}-#{RUBY_PATCHLEVEL}",
+      :uptime => Time.now.to_i - @started_at
+    }
   end
 
   def start
     Thread.new {
-      Net::HTTP::Server.run(:port => 8080) do |request,stream|
-        logger.unknown "Responder request: #{request}"
-        [200, {'Content-Type' => 'application/json'}, [INFO.to_json]]
+      @logger.unknown "Start responder 0.0.0.0:#{@port}"
+      Net::HTTP::Server.run(:port => @port) do |request, stream|
+        @logger.unknown "Responder request: #{request}"
+        [200, {'Content-Type' => 'application/json'}, [info.to_json]]
       end
     }
   end
--- lib/riemann/babbler/version.rb
+++ lib/riemann/babbler/version.rb
@@ -1,5 +1,5 @@
 module Riemann
   class Babbler
-    VERSION = '1.2.5'
+    VERSION = '1.2.6'
   end
 end
