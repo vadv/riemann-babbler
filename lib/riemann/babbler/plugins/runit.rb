728744f2dbd3e4e0aa232717332da7032159bcc7
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-08-20 13:23:59 +0400

    new release

--- lib/riemann/babbler/plugins/runit.rb
+++ lib/riemann/babbler/plugins/runit.rb
@@ -21,15 +21,15 @@ class Riemann::Babbler::Runit < Riemann::Babbler
   def read_run_status
     status = Array.new
     Dir.glob('/etc/service/*').each do |srv|
-      human_srv = ' ' + srv.gsub(/\/etc\/service\//, '')
+      human_srv = srv.gsub(/\/etc\/service\//, '')
       next if plugin.not_monit.include? human_srv
       stat_file = File.join(srv, 'supervise', 'stat')
       next unless File.exists? stat_file
       srv_uptime = uptime(srv)
       if (File.read( stat_file ).strip == 'run') && (srv_uptime > plugin.uptime)
-        status << {:service => plugin.service + human_srv , :state => 'ok', :description => "runit service #{human_srv} running", :metric => srv_uptime}
+        status << {:service => plugin.service + ' ' + human_srv , :state => 'ok', :description => "runit service #{human_srv} running", :metric => srv_uptime}
       else
-        status << {:service => plugin.service + human_srv , :state => 'critical', :description => "runit service #{human_srv} not running", :metric => srv_uptime}
+        status << {:service => plugin.service + ' ' + human_srv , :state => 'critical', :description => "runit service #{human_srv} not running", :metric => srv_uptime}
       end
     end
     status
--- lib/riemann/babbler/version.rb
+++ lib/riemann/babbler/version.rb
@@ -1,5 +1,5 @@
 module Riemann
   class Babbler
-    VERSION = '1.2.3'
+    VERSION = '1.2.4'
   end
 end
