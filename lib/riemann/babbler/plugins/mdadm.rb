03a255c5cd3ecbe7a9d4a865f5a6a1a54d4f539e
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-09-02 16:27:37 +0400

    fixes in mdadm status

--- lib/riemann/babbler/plugins/mdadm.rb
+++ lib/riemann/babbler/plugins/mdadm.rb
@@ -10,6 +10,11 @@ class Riemann::Babbler::Mdadm < Riemann::Babbler
     File.exists? '/proc/mdstat'
   end
 
+  def mdadm_status_well?(text)
+    text = text.gsub('[','').gsub(']','')
+    text.gsub(/U/,'').empty?
+  end
+
   def collect 
     file = File.read('/proc/mdstat').split("\n")
     status = Array.new
@@ -19,7 +24,7 @@ class Riemann::Babbler::Mdadm < Riemann::Babbler
       device = file[index-1].split(':')[0].strip
 
       mdstatus = line.split(" ").last
-      next if mdstatus == '[UU]' # пропускаем все збс
+      next if mdadm_status_well?(mdstatus) # пропускаем все збс
       if mdstatus == plugin.states.send(device).to_s # пропускаем если стейт зафикисирован в конфиге
         status << { :service => plugin.service + " #{device}", :metric => 1, :state => 'ok', :description => "mdadm failed device #{device}, but disabled in config" }
         next
