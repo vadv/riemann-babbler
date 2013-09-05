245567bb6ba58f82b4c8c62ab25141d8531c17ed
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-09-05 14:59:28 +0400

    extend IO

--- lib/riemann/babbler/support/monkey_patches.rb
+++ lib/riemann/babbler/support/monkey_patches.rb
@@ -21,3 +21,21 @@ module Beefcake
     
   end
 end
+
+class IO
+  TAIL_BUF_LENGTH = 1 << 16
+
+  def tail(n)
+    return [] if n < 1
+
+    seek -TAIL_BUF_LENGTH, SEEK_END
+
+    buf = ""
+    while buf.count("\n") <= n
+      buf = read(TAIL_BUF_LENGTH) + buf
+      seek 2 * -TAIL_BUF_LENGTH, SEEK_CUR
+    end
+
+    buf.split("\n")[-n..-1]
+  end
+end
