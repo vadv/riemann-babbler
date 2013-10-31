e45750215925c57542fa3c6d9faa8dff494996d3
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-10-31 12:10:25 +0400

    minimize count of events

--- lib/riemann/babbler/plugin.rb
+++ lib/riemann/babbler/plugin.rb
@@ -29,11 +29,13 @@ module Riemann
       attr_reader :riemann, :plugin_name, :plugin, :errors
 
       def initialize(riemann)
-        @riemann     = riemann
-        @storage     = Hash.new
-        @plugin_name = name_to_underscore(self.class.name)
-        @plugin      = opts.plugins.send(plugin_name)
-        @errors      = opts.errors.send(plugin_name)
+        @riemann                = riemann
+        @storage                = Hash.new
+        @storage['last_state']  = Hash.new
+        @storage['last_metric'] = Hash.new
+        @plugin_name            = name_to_underscore(self.class.name)
+        @plugin                 = opts.plugins.send(plugin_name)
+        @errors                 = opts.errors.send(plugin_name)
         set_default
         init
       end
@@ -71,25 +73,29 @@ module Riemann
         report_with_diff(event) and return if event[:as_diff]
         event[:metric] = event[:metric].round(2) if event[:metric].kind_of? Float
         event[:state] = get_state(event)
-        riemann << event
+        riemann << event if not_minimize_sent_event(event)
+        set_last_event(event)
       end
+
+
+      ### Helper for reports ###
 
       def report_with_diff(event)
         current_metric = event[:metric]
-        old_metric     = @storage[event[:service]]
+        old_metric     = @storage['last_metric'][event[:service]]
         if old_metric && current_metric + old_metric < 2**64
           event[:metric] = current_metric - old_metric
           event.delete(:as_diff)
           report(event)
         end
-        @storage[event[:service]] = current_metric
+        @storage['last_metric'][event[:service]] = current_metric
       end
 
       #@return state
       def get_state(event)
         return event[:state] if event[:state]
-        metric = event[:metric].to_f
-        return State::CRITICAL if metric.nil? #todo: неправильно здесь возврашать nil, это ошибка плагина!!!
+        return event[:state] if event[:metric].nil?
+        metric   = event[:metric].to_f
         warning  = plugin.states.warning.nil? ? nil : plugin.states.warning
         critical = plugin.states.critical.nil? ? nil : plugin.states.critical
         return State::OK if (warning || critical).nil?
@@ -111,6 +117,22 @@ module Riemann
         end
       end
 
+      #@return true if event may be sended
+      def not_minimize_sent_event(event)
+        return true if !opts.riemann.minimize_event_count # нет задачи минизировать
+        return true if event[:metric]                     # если есть метрика - надо отослать graphite
+        return true if event[:state] != State::OK         # все предупреждения отсылаем
+        return true if @storage['last_state'][event[:service]] != State::OK
+        log :debug, "Skip send event #{event}"
+        false
+      end
+
+      def set_last_event(event)
+        # тут пока только last_state
+        @storage['last_state'][event[:service]] = event[:state] if opts.riemann.minimize_event_count
+      end
+
+      ### Errors ###
       def plugin_error!(msg)
         errors.count += 1
         log :error, "#{plugin.service} error num #{errors.count}:\n #{msg}"
--- lib/riemann/babbler/version.rb
+++ lib/riemann/babbler/version.rb
@@ -1,5 +1,5 @@
 module Riemann
   module Babbler
-    VERSION = '2.0.0pre10'
+    VERSION = '2.0.0pre11'
   end
 end
