3a888ea431a3e5f15eac95629129034df5599902
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-11-13 01:27:01 +0400

    [ups] udp responder start at 127.0.0.1 by default

--- lib/riemann/babbler/plugins/responder_udp.rb
+++ lib/riemann/babbler/plugins/responder_udp.rb
@@ -4,6 +4,7 @@ class Riemann::Babbler::Plugin::ResponderUdp < Riemann::Babbler::Plugin
 
   def init
     plugin.set_default(:port, opts.riemann.responder_udp_port)
+    plugin.set_default(:host, '127.0.0.1')
   end
 
   def process(data, src)
@@ -17,8 +18,8 @@ class Riemann::Babbler::Plugin::ResponderUdp < Riemann::Babbler::Plugin
   end
 
   def run!
-    log :unknown, "Start udp server at #{plugin.port}"
-    Socket.udp_server_loop(plugin.port) do |data, src|
+    log :unknown, "Start udp server at #{plugin.host}:#{plugin.port}"
+    Socket.udp_server_loop(plugin.host, plugin.port) do |data, src|
       log :debug, "Recived data: #{data.inspect}, from client: #{src.inspect}"
       process(data, src) 
     end
--- lib/riemann/babbler/version.rb
+++ lib/riemann/babbler/version.rb
@@ -1,5 +1,5 @@
 module Riemann
   module Babbler
-    VERSION = '2.1.1'
+    VERSION = '2.1.2'
   end
 end
