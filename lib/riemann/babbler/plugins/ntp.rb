33d1514b2d3b67631d89db105345a6149161efe2
Vasiliev Dmitry <vadv.mkn@gmail.com>
2013-11-19 15:26:54 +0400

    npt timeout parametrs

--- lib/riemann/babbler/plugins/ntp.rb
+++ lib/riemann/babbler/plugins/ntp.rb
@@ -5,19 +5,16 @@ class Riemann::Babbler::Plugin::Ntp < Riemann::Babbler::Plugin
   def init
     plugin.set_default(:service, 'ntp')
     plugin.set_default(:host, 'pool.ntp.org')
+    plugin.set_default(:server_timeout, 30)
     plugin.states.set_default(:warning, 5)
     plugin.states.set_default(:critical, 10)
-  end
-
-  def diff
-    Net::NTP.get(plugin.host, 'ntp', 5).time.to_i - Time.now.to_i
   end
 
   def collect
     {
       :service     => plugin.service + " #{plugin.host}",
       :description => "Ntp lag with host #{plugin.host}",
-      :metric      => (::Net::NTP.get(plugin.host, 'ntp', 5).time.to_f - Time.now.to_f).abs
+      :metric      => (::Net::NTP.get(plugin.host, 'ntp', plugin.server_timeout).time.to_f - Time.now.to_f).abs
     }
   end
 
--- lib/riemann/babbler/version.rb
+++ lib/riemann/babbler/version.rb
@@ -1,5 +1,5 @@
 module Riemann
   module Babbler
-    VERSION = '2.1.3.2'
+    VERSION = '2.1.4'
   end
 end
